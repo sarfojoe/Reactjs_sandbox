131a6928dec2d1271822111ced695d751e156af3
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-03-25 14:32:49 +0200

    props changed to state

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
-
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, World</h1>
-      <p>Hello world</p>
-      <Person name="Joe" age="39" />
-      <Person name="Sarfo" age="28" />
-      <Person name="Myra" age="40" />
-      <Person name="Frema" age="43" />
-      <Person name="Nana" age="15" />
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Joe', age: 39},
+      { name: 'Sarfo', age: 28},
+      { name: 'Myra', age: 40},
+      { name: 'Frema', age: 43},
+      { name: 'Nana', age: 15},
+    ]
+  };
+
+  render () {
+    return (
+      <div>
+        <h1>Hello, World</h1>
+        <p>Hello world</p>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+        <Person name={this.state.persons[4].name} age={this.state.persons[4].age} />
 
     </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
