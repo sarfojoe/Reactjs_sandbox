b0aa425737e3ebf864ae4950fb1142c8a35ccacb
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-03-25 14:01:25 +0200

    porps added

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -6,11 +6,16 @@ import Person from './Person/Person'
 const App = () => {
   return (
     <div>
-    <h1>Hello, World</h1>
-    <p>Hello world</p>
-    <Person />
+      <h1>Hello, World</h1>
+      <p>Hello world</p>
+      <Person name="Joe" age="39" />
+      <Person name="Sarfo" age="28" />
+      <Person name="Myra" age="40" />
+      <Person name="Frema" age="43" />
+      <Person name="Nana" age="15" />
+
     </div>
   );
-}
+};
 
 export default App;
