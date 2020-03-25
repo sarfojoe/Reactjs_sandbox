be8dc3b4c6dc31b6e609a927d3dde92222c21cd6
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-03-25 13:42:19 +0200

    update

---
+++ 2_task/src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Person.css';
+
+const Person = () => {
+    return (
+        <div className='card'>
+        <p>Hello, my name is Joseph and I am 39 Years old</p>
+        </div>
+    )
+};
+export default Person;
