2b42d6e007b5ae0e0c83aca30f4f54c61041d8de
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-05-20 22:25:21 +0300

    remove post added and hosted at firebase

--- 10_task/src/Components/Nav/Nav.js
+++ 10_task/src/Components/Nav/Nav.js
@@ -1,36 +1,3 @@
-/* import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-router-dom";
-
-const MainNav = () => {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="/">Joseph Boadi</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-          <Link className="d-inline p-2 bg-dark text-white" to="/">
-            Home
-          </Link>
-          <Link className="d-inline p-2 bg-dark text-white" to="/about">
-            About Me
-          </Link>
-          <Link className="d-inline p-2 bg-dark text-white" to="/Blog">
-            Blog
-          </Link>
-          <Link className="d-inline p-2 bg-dark text-white" to="/NewPost">
-            New Post
-          </Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
-
-export default MainNav;
- */
-
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, Form, Button, FormControl } from "react-bootstrap";
@@ -56,7 +23,7 @@ const Header = () => {
             Blog
           </Link>
           <Link className="d-inline p-2 text-white" to="/NewPost">
-            New Post
+            Suggestion
           </Link>
         </Nav>
         <Form inline>
